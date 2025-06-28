0c0b8f0707083b1f6bcdc36d40d7b4b39ae111a4
HiwinsDaisin27 <hiwinsdaisinxb@gmail.com>
2025-06-29 01:54:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,9 @@ if "chat_session" not in st.session_state:
 st.title("🧠 Generative AI Chatbot")
 st.sidebar.image("https://cdn-icons-png.flaticon.com/512/4712/4712102.png", width=100)
 st.sidebar.markdown("### 🤖 Your AI Side Chick")
-st.sidebar.markdown(" Built with Python, Streamlit, and love 💻🤍 
+st.sidebar.markdown("Built with Python, Streamlit , and love 💻🤍
 ")
+
 
 if st.sidebar.button("🔄 Reset Chat"):
     st.session_state.chat_session = model.start_chat(history=[])
